ca7576809e1463e50729d46a28d7cab1d9221a8d
GlaysonRibeiro <glayson_ribeiro@jabil.com>
2023-03-29 15:50:56 -0300

    Primitivos js

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-console.log("rodando o add on do blender")
-console.log("rodando o Phyton")
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaomatematica = primeiroNumero + segundoNumero
+
+console.log(operacaomatematica)
+
+// Ponto Flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+const Alura = "Alura"
+
+console.log(primeiroNumero*Alura)
