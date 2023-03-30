eff344931c96d2600154e8049d47a26282128e6d
GlaysonRibeiro <glayson_ribeiro@jabil.com>
2023-03-30 13:27:15 -0300

    salvando conteúdo do curso Java Script

--- Type-Number.js
+++ Type-Number.js
@@ -19,3 +19,22 @@ console.log(novaOperacao)
 const Alura = "Alura"
 
 console.log(primeiroNumero*Alura)
+
+const idade = 2;
+const nome = "lara";
+const sobrenome = "Ayumi";
+const bacharel = false;
+const dezAnosDepois = "12";
+const nomeCompleto = nome + sobrenome;
+const resultado = idade + dezAnosDepois;
+
+console.log(nomeCompleto)
+console.log(resultado)
+console.log(bacharel == 1)
+
+const soma = 34 + 56;
+const multiplicacao = soma * 5;
+const subtracao = (soma * 5) - 25;
+const operação = subtracao / 2;
+
+console.log(subtracao)
