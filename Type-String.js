c526a84f4575a52c34c99bc5a69123e18d4df564
GlaysonRibeiro <glayson_ribeiro@jabil.com>
2023-03-30 10:21:34 -0300

    adicionando conteudo do curso Js

---
+++ Type-String.js
@@ -0,0 +1,51 @@
+const texto1 = "Olá mundo";
+const texto2 = 'Olá mundo';
+const senha = "senhaDoBode123@";
+const StringdeNumeros = "123456789";
+
+const citacao = "Meu nome é ";
+const meuNome = "Glayson";
+
+// Codificação de String 
+
+var cifrao = "\u0024"
+var aMaiusculo = "\u0041"
+var tique = "\u2705"
+var hiragana = "\u3041"
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// trabalhando com String 
+
+var cidade = "belo horizonte";
+var input = "Belo Horizonte";
+
+console.log(cidade === input)
+
+var inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo)
+
+var senha2 = "minhasenha123";
+
+console.log(senha2.length)
+
+
+// Concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template Literal 
+
+var nome = "Glayson";
+var profissão = "Programador";
+console.log(`Meu nome é ${nome} e eu sou um ${profissão}`);
+
+// cotatenação de Strings 
+
+// Var nome = "Bodex";
+// var idade = 29;
+// console.log("meu nome é " + nome + " eu tenho " + idade + " anos");
