793b5052a2e8f1c943a7e62b2ac328ef93625c5e
GlaysonRibeiro <glayson_ribeiro@jabil.com>
2023-03-31 12:37:02 -0300

    Estudo de conversão Java Script

--- variaveis.js
+++ variaveis.js
@@ -1,4 +1,4 @@
-// var 
+// --- var 
 
 // var autura = 5;
 // var comprimento = 7;
@@ -20,15 +20,84 @@
 
 // console.log(area)
 
-const forma = 'triangulo';
-const altura = 5;
-const comprimento = 7;
-let area;
+// const forma = 'triangulo';
+// const altura = 5;
+// const comprimento = 7;
+// let area;
 
-if (forma === 'quadrado') {
-    area = altura * comprimento;
-}   else {
-    area = (altura * comprimento) / 2;
-}
+// if (forma === 'quadrado') {
+//     area = altura * comprimento;
+// }   else {
+//     area = (altura * comprimento) / 2;
+// }
 
-console.log(area)
+// console.log(area)
+
+
+// --- Booleans 
+
+// const usuarioLogado = true
+// const contaPaga = false
+
+// --- truth ou falsy 
+
+// 0 => false 
+//1 => true 
+
+// Aonsole.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+// let minhaVar;
+// let varNull = null
+
+// let numero = 3;
+// let texto = "Alura"
+
+// console.log(minhaVar)
+// console.log(varNull)
+
+
+// console.log(typeof numero)
+// console.log(typeof texto)
+
+// console.log(typeof minhaVar)
+// console.log(typeof varNull)
+
+// --- Conversões implicita
+
+// const nuremo1 = 345;
+// const numeroString1 = "345";
+
+// console.log(nuremo1 === numeroString1) // 3 iguais compara o tipo -Boolean 345 === String "345" = False
+
+// const nuremo2 = 345;
+// const numeroString2 = "345";
+
+// console.log(nuremo2 == numeroString2) // 2 uguais compara o conteuro Booleam 345 == String "345"
+// console.log(nuremo1 + numeroString1)
+
+// console.log(nuremo1 + nuremo2)
+
+// Conversão explicita Number() e String()
+
+const nuremo1 = 345;
+const numeroString1 = "345";
+
+
+const nuremo2 = 345;
+const numeroString2 = "345";
+
+console.log(nuremo1 === Number(numeroString1)) // 
+
+console.log(nuremo1 === String(nuremo2))
+
+console.log(nuremo2 == numeroString2)  
+
+console.log(nuremo1 + numeroString1)
+
+console.log(nuremo2 + Number(numeroString2))
+
+console.log(String(nuremo1) + String(nuremo2))
+
+console.log(Number(numeroString1) + Number(numeroString2))
