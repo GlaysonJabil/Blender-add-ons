31ddddb126693220e96716a05f19b25db11bee06
GlaysonRibeiro <glayson_ribeiro@jabil.com>
2023-04-04 16:57:02 -0300

    criando exemplos de Fumnctions

---
+++ Function.js
@@ -0,0 +1,11 @@
+// ----- FUNÇÔES -----
+// 1 declara a Function 
+
+function imprimetexto(texto) {
+    console.log(texto)
+}
+
+// 2 executa a Function ( 1 ou mais vezes)
+
+imprimetexto("oi");
+imprimetexto("Colé Bodogs")
